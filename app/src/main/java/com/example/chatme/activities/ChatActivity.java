cea992bf83b1ebf7221d0fd4639dffebc5f00804
MahmoudMostafa580 <mahmoud.mostafa20112011@gmail.com>
2021-08-31 18:30:22 +0200

    prepareChatLayout

---
+++ app/src/main/java/com/example/chatme/activities/ChatActivity.java
@@ -0,0 +1,16 @@
+package com.example.chatme.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.chatme.R;
+
+public class ChatActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat);
+    }
+}
